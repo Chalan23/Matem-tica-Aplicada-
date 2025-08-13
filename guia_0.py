149b47d24a7cd222f769c300e83ace939b76c991
Chalan23 <ernesto.vilches.chalan@gmail.com>
2025-08-13 09:46:43 -0400

    mate

---
+++ guia_0.py
@@ -0,0 +1,21 @@
+# # Escribe un código que pida al usuario ingresar un número real y determine 
+# # si el número ingresado es positivo, negativo o cero.
+
+# numero = float(input("Ingresa un numero: "))
+
+# if numero > 0:
+#     print("El numero es positivo.")
+# elif numero < 0:
+#     print("El numero es negativo.")
+# else:
+#     print("El numero es cero.")
+
+
+# Escribe un código que pida al usuario ingresar un número entero y 
+# que determine si el número ingresado es par o impar.
+
+numero = int(input("Ingresa un numero: "))
+if numero % 2 == 0:
+    print("El numero es par.")
+else:
+    print("El numero es impar.")
